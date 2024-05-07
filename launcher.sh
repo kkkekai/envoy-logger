cadcee2b555061e31e335d299e378ddb6918e9c1
RLD <ledisez.romain@gmail.com>
2024-05-07 16:37:01 +0200

    Launcher changes

--- launcher.sh
+++ launcher.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "Installing"
-python3 -m pip install -U setuptools
-python3 -m pip install --force-reinstall git+https://github.com/amykyta3/envoy-logger
+python3 -m pipx install -U setuptools
+python3 -m pipx install --force-reinstall git+https://github.com/amykyta3/envoy-logger
 
 echo "Starting logger"
 python3 -m envoy_logger $ENVOY_LOGGER_CFG_PATH
